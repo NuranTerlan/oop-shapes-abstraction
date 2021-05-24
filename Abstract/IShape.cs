505417a032339ec2e4fbcc9ff556af54a1fef936
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-05-24 12:25:33 +0400

    migrated to abstract class

--- Abstract/IShape.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OOP_Tasks_Day1.Abstract
-{
-    public interface IShape
-    {
-        double Length { get; set; }
-        double AreaVar { get; set; }
-        double CalculateArea();
-    }
-}
