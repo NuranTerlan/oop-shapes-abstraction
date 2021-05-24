c46c091d074b38d1fc97ceefa37e30961c9bc66e
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-05-24 12:25:50 +0400

    IShape migrated to Shape abstract class

---
+++ Abstract/Shape.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OOP_Tasks_Day1.Abstract
+{
+    public abstract class Shape
+    {
+        protected string IAmXD; 
+        public double SideLength { get; set; }
+        public abstract double CalculateArea();
+
+        public virtual string ShapeStringify(Shape shape)
+        {
+            // extract class name from full assembly name
+            // X.X.X.X.Circle -> objName: Circle
+            // [^1] specifies last
+            string objName = shape.GetType().ToString().Split('.')[^1];
+            return $"{objName} ({IAmXD} ({SideLength})) ==> Surface area = {Math.Round(CalculateArea(), 2)}" +
+                   $"\nI am a {objName} shape";
+        }
+
+        public override string ToString()
+        {
+            return ShapeStringify(this);
+        }
+    }
+}
